6eba06595502007749d30919ac95d766a3a24e1b
matheus97sm <matheus97sm@gmail.com>
2021-09-07 21:11:43 -0300

    Responsivo banner, sobre e menu

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
 
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, minimal-ui" />
 
   <title><?php wp_title( '|', true, 'right' ); ?></title>
 
